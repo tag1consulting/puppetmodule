a7ac321a4ec3ffb793179978817bc3de40b4507b
Daniel Schaaff <dschaaff@intellisis.com>
2015-08-18 17:23:16 -0700

    switch to appdmg package provider for mac

--- manifests/params.pp
+++ manifests/params.pp
@@ -93,7 +93,7 @@ class puppet::params {
     'Darwin': {
       $puppet_agent_service         = 'com.puppetlabs.puppet'
       $puppet_agent_package         = 'puppet'
-      $package_provider             = 'apple'
+      $package_provider             = 'appdmg'
       $puppet_conf                  = '/etc/puppet/puppet.conf'
       $puppet_vardir                = '/var/lib/puppet'
       $puppet_ssldir                = '/etc/puppet/ssl'
